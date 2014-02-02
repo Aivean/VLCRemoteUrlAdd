5fea263d348113cdeb5537abccbfd1e811b97ddc
Aivean <aiveeen@gmail.com>
2014-02-02 02:39:59 +0200

    Fixed VLCController

--- src/main/java/com/aivean/socketclient/VLCController.java
+++ src/main/java/com/aivean/socketclient/VLCController.java
@@ -13,34 +13,32 @@ public class VLCController {
     public String enqueue(String[] urls) {
 
         Process p;
-        StringBuffer output = new StringBuffer();
+        StringBuilder output = new StringBuilder();
 
         try {
             p = Runtime.getRuntime().exec("nc -U " + Config.SOCKET);
+            InputStream inputStream = p.getInputStream();
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
             for (String url : urls) {
                 out.write("enqueue " + url + "\n");
+                out.flush();
+            }
+            out.close();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
             }
 
             p.waitFor();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine())!= null) {
-                output.append(line + "\n");
-            }
-
-            out.close();
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         return output.toString();
     }
-
-
 }
