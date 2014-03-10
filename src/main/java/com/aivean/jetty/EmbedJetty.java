7ce301d551023e2b701aee9a4feff18ca4a985fd
Aivean <aiveeen@gmail.com>
2014-03-10 23:08:18 +0200

    Fixed maven packaging as WAR and runnable JAR (for jetty). Now WAR is default and JAR can be enabled via profile "jetty".

--- src/main/java/com/aivean/jetty/EmbedJetty.java
+++ src/main/java/com/aivean/jetty/EmbedJetty.java
@@ -4,16 +4,31 @@ import com.aivean.config.Config;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+
 /**
  * @author <a href="mailto:ivan.zaytsev@webamg.com">Ivan Zaytsev</a>
  *         2014-02-01
  */
 public class EmbedJetty {
 
+    private static String getJarUrl() {
+        URL url = EmbedJetty.class.getProtectionDomain().getCodeSource().getLocation();
+        try {
+            return URLDecoder.decode(url.getFile(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Server server = new Server(Config.PORT);
 
-        String rootPath = EmbedJetty.class.getClassLoader().getResource("webapp").toString();
+        URL resource = EmbedJetty.class.getClassLoader().getResource(".");
+        String rootPath = resource == null ? getJarUrl() : resource.toString();
+        System.out.println(rootPath);
         WebAppContext webapp = new WebAppContext(rootPath, "");
         server.setHandler(webapp);
 
